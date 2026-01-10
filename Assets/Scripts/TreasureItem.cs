31bd79a4e1b45cf7cf1dcf410ddaf8246d2b8f10
Dawson <30555174+Dawson12236@users.noreply.github.com>
2026-01-10 14:18:28 -0500

    Adjusted, and Added Audio To Coin Collection. Added Audio To Player For Taking Damage. Added AudioManager. Added and Set Up Main Campus Scene.

--- Assets/Scripts/TreasureItem.cs
+++ Assets/Scripts/TreasureItem.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TreasureItem : MonoBehaviour // Meant to be used for a test case, testing
 {
     public int treasureAmount = 1;
-    private void OnTriggerEnter2D(Collider2D collision) // I don't want to know why, but removing the treasure from the scene through this class was the only way to remove the coins.
+    private AudioSource coinAudio;
+    private bool coinCollected = false;
+
+    void Awake()
     {
-        if(collision.gameObject.tag == "Player")
+        coinAudio = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if(coinCollected && !coinAudio.isPlaying)
         {
             Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision) // I don't want to know why, but removing the treasure from the scene through this class was the only way to remove the coins.
+    {
+        if(!coinCollected)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                coinAudio.Play();
+                GetComponent<SpriteRenderer>().enabled = false;
+                coinCollected = true;
+            }
         }
     }
 }
