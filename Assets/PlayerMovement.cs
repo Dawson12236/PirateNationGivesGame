2faa0965ce94d9a2485ecec33f3435a2b6e60146
Deon Davis <davisde23@students.ecu.edu>
2026-01-12 17:02:23 -0500

    State Machine & Revised Input Recognition
    Implemented a state machine with the following states: Idle, Walk, Jump, Fall, and Crouch.
    
    Each state has an update function that processes what should happen in that state. For example, the walk state moves the player based on the vector of the move action from the input system asset, which is assigned to the moveInput variable.
    
    Each state will be able to do whatever it needs to upon entering and exiting its states. For example, the jump state adds force to the player to simulate jumping, but this only happens upon entering the state.
    
    Revised the previous input recognition due to confusion and lack of control when using state machines
    
    Redid crouching to accommodate for the implementation of a state machine. the wants The crouch boolean has been removed, and crouching is triggered when in the walk or idle states upon pressing the crouch button.
    
    Speed has been removed in favor of two new variables that dictate the speed of the walk and crouch states named walk_speed and crouch_speed.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
 public class PlayerMovement : MonoBehaviour
 {
-    private InputSystem_Actions controls;
+
+    enum Movement_State {Idle, Walk, Jump, Fall, Crouch};
+    Movement_State current_state;
+    Movement_State previous_state;
+
+    public InputActionAsset InputActions;
+
+    private InputAction jump_action;
+    private InputAction move_action;
+    private InputAction crouch_action;
+
     private Rigidbody2D rb;
     private Vector2 movingInput;
     public LayerMask groundLayer;
@@ -19,96 +30,176 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 standingSize;
     public Vector2 crouchingSize;
     public float crouchHeight, standingHeight;
-    private bool wantsCrouch;
 
-    public float speed = 5f;
+    public float walk_speed = 5f;
+    public float crouch_speed = 2f;
     private float jump = 10f;
     bool isGrounded;
     private float blocks = 0f;
-
     private void Awake()
     {
-        controls = new InputSystem_Actions();
-        controls.Player.Enable(); 
-    }
-
-    void Start()
-    {
-        controls.Player.Move.performed += OnMove;
-        controls.Player.Move.canceled += OnMove;
-        controls.Player.Jump.performed += OnJump;
-        controls.Player.Crouch.performed += OnCrouch;
-        controls.Player.Crouch.canceled += OnCrouch;
-        rb = GetComponent<Rigidbody2D>(); 
+        move_action = InputSystem.actions.FindAction("Move");
+        jump_action = InputSystem.actions.FindAction("Jump");
+        crouch_action = InputSystem.actions.FindAction("crouch");
+        rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
     }
 
     void Update()
     {
-        //rb.linearVelocity = movingInput * speed;
+        movingInput = move_action.ReadValue<Vector2>();
+        switch (current_state)
+        {
+            case Movement_State.Idle:
+                UpdateIdleState();
+                break;
+            case Movement_State.Walk:
+                UpdateWalkState(); 
+                break;
+            case Movement_State.Jump:
+                UpdateJumpState();
+                break;
+            case Movement_State.Fall:
+                UpdateFallState();
+                break;
+            case Movement_State.Crouch:
+                UpdateCrouchState();
+                break;
+        }
     }
 
-    private void FixedUpdate()
+    void EnterState(Movement_State new_state)
     {
-        if (movingInput != Vector2.zero) 
+        ExitState();
+        current_state = new_state;
+        switch (current_state)
         {
-            rb.linearVelocity = new Vector2(movingInput.x * speed, rb.linearVelocity.y);
+            case Movement_State.Idle:
+                print("Entered Idle State");
+                break;
+            case Movement_State.Walk:
+                print("Entered Walk State");
+                break;
+            case Movement_State.Jump:
+                print("Entered Jump State");
+                rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+                break;
+            case Movement_State.Fall:
+                print("Entered Fall State");
+                break;
+            case Movement_State.Crouch:
+                print("Entered Crouch State");
+                playerCollider.size = new Vector2(playerCollider.size.x, crouchHeight);
+                playerCollider.offset = new Vector2(0, -0.5f);
+                break;
+        }
+    }
+
+    void ExitState()
+    {
+        previous_state = current_state;
+        switch (current_state)
+        {
+            case Movement_State.Idle:
+                print("Exited Idle State");
+                break;
+            case Movement_State.Walk:
+                print("Exited Walk State");
+                break;
+            case Movement_State.Jump:
+                print("Exited Jump State");
+                break;
+            case Movement_State.Fall:
+                print("Exited Fall State");
+                break;
+            case Movement_State.Crouch:
+                print("Exited Crouch State");
+                break;
         }
 
-        if(wantsCrouch && isGrounded)
+    }
+
+    void UpdateIdleState()
+    {
+        print("Idling");
+        if (!isGrounded && rb.linearVelocity.y < 0)
         {
-            Crouch();
+            EnterState(Movement_State.Fall);
         }
-        else
+        if (movingInput != Vector2.zero)
+        {
+            EnterState(Movement_State.Walk);
+        }
+        if (jump_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Jump);
+        }
+        if (crouch_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Crouch);
+        }
+    }
+
+    void UpdateFallState()
+    {
+        print("Falling");
+        if (isGrounded)
+        {
+            EnterState(Movement_State.Idle);
+        }
+    }
+
+    void UpdateWalkState()
+    {
+        rb.linearVelocity = new Vector2(movingInput.x * walk_speed, rb.linearVelocity.y);
+        print("Walking");
+        if (!isGrounded && rb.linearVelocity.y < 0)
+        {
+            EnterState(Movement_State.Fall);
+        }
+        if (movingInput == Vector2.zero)
+        {
+            EnterState(Movement_State.Idle);
+        }
+        if (jump_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Jump);
+        }
+        if (crouch_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Crouch);
+        }
+    }
+
+    void UpdateJumpState()
+    {
+        print("Jumping");
+        if (rb.linearVelocity.y < 0)
+        {
+            EnterState(Movement_State.Fall);
+        }
+    }
+
+    void UpdateCrouchState()
+    {
+        print("Crouching");
+        rb.linearVelocity = new Vector2(movingInput.x * crouch_speed, rb.linearVelocity.y);
+        if (!crouch_action.IsPressed())
         {
             StandAttempt();
         }
-        
+
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        movingInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnCrouch(InputAction.CallbackContext context)
-    {
-        if (context.performed && isGrounded)
-        {
-            wantsCrouch = true;
-        }
-        else
-        {
-            wantsCrouch = false;
-        }
-    }
-
-    public void Crouch()
-    {
-        playerCollider.size = new Vector2(playerCollider.size.x, crouchHeight);
-        playerCollider.offset = new Vector2(0, -0.5f);
-        speed = 4f;
-    }
     public void StandAttempt()
     {
         bool notblocked = !Physics2D.OverlapCircle(headCheck.position, headCheckRadius, groundLayer);
         if (notblocked)
         {
-            Debug.Log("uncrouched");
             playerCollider.size = new Vector2(playerCollider.size.x, standingHeight);
             playerCollider.offset = new Vector2(0, 0);
-            speed = 5f;
-        }
-    }
-
-   
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (context.performed && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            Debug.Log("Jumping");
+            EnterState(Movement_State.Idle);
         }
     }
 
