96b9aff13f4cc8ff13b7ff747936bc189b779fa6
LoganOC77 <loganoconnel194@gmail.com>
2026-01-14 06:59:58 -0500

    Added coin scattering
    This is part one, will do part 2 later

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,163 +1,267 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Events;
-using UnityEngine.Rendering;
 
 
 public class PlayerMovement : MonoBehaviour
 {
-    private InputSystem_Actions controls;
+    enum Movement_State { Idle, Walk, Jump, Fall, Crouch};
+    Movement_State current_state;
+    Movement_State previous_state;
+    public InputActionAsset InputActions;
+    private InputAction jump_action;
+    private InputAction move_action;
+    private InputAction crouch_action;
+
+    public GameObject manager;
+    
+    private AudioSource peeDeeAudio;
+    public AudioClip damageTaken;
     private Rigidbody2D rb;
-    private Vector2 movingInput;
+    public Transform headCheck;
+    public CapsuleCollider2D playerCollider;
     public LayerMask groundLayer;
 
-
-    public Transform headCheck;
-    public float headCheckRadius = 0.2f;
-
-    public CapsuleCollider2D playerCollider;
     public Vector2 standingSize;
     public Vector2 crouchingSize;
     public float crouchHeight, standingHeight;
-    private bool wantsCrouch;
     public Vector2 standingOffset;
     public Vector2 crouchingOffset;
-
-    public float speed = 5f;
-    private float jump = 10f;
+    public float headCheckRadius = 0.2f;
+    public float walk_speed = 5f;
+    public float w_acceleration = 50f;
+    public float crouch_speed = 2f;
+    public float c_acceleration = 35f;
+    public float jump_strength = 125f;
+    private float friction_coefficient = 1f;
     public float recoilHorizontalStrength = 7f;
     public float recoilVerticalStrength = 7f;
-    public float standardGravity = 2f;
-    public float recoilGravity = 3f;
+    public float standardGravity = 3f;
+    public float recoilGravity = 5f;
     bool isGrounded;
     bool tookDamage = false;
-    private float blocks = 0f;
+    bool playerTookDamageFromRight = true;
+    private Vector2 movingInput;
 
     private void Awake()
     {
-        controls = new InputSystem_Actions();
-        controls.Player.Enable(); 
-    }
-
-    void Start()
-    {
-        controls.Player.Move.performed += OnMove;
-        controls.Player.Move.canceled += OnMove;
-        controls.Player.Jump.performed += OnJump;
-        controls.Player.Crouch.performed += OnCrouch;
-        controls.Player.Crouch.canceled += OnCrouch;
-        rb = GetComponent<Rigidbody2D>(); 
+        move_action = InputSystem.actions.FindAction("Move");
+        jump_action = InputSystem.actions.FindAction("Jump");
+        crouch_action = InputSystem.actions.FindAction("crouch");
+        peeDeeAudio = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     void Update()
     {
-        //rb.linearVelocity = movingInput * speed;
+        movingInput = move_action.ReadValue<Vector2>(); // Assigns movingInput to the move_action's vector its retriving from directional inputs (Joysticks, WASD, or Arrow Keys)
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, movingInput.x * walk_speed, w_acceleration * Time.deltaTime);
+        switch (current_state) // This switch statement switches to the corresponding update function depending on the current state
+        {
+            case Movement_State.Idle:
+                UpdateIdleState();
+                break;
+            case Movement_State.Walk:
+                UpdateWalkState();
+                break;
+            case Movement_State.Jump:
+                UpdateJumpState();
+                break;
+            case Movement_State.Fall:
+                UpdateFallState();
+                break;
+            case Movement_State.Crouch:
+                UpdateCrouchState();
+                break;
+        }
     }
 
-    private void FixedUpdate()
+    void EnterState(Movement_State new_state)
     {
-        if (movingInput != Vector2.zero) 
+        ExitState();
+        current_state = new_state;
+        switch (current_state) // This switch statement executes the corresponding lines once entering a state
         {
-            rb.linearVelocity = new Vector2(movingInput.x * speed, rb.linearVelocity.y);
+            case Movement_State.Idle:
+                print("Entered Idle State");
+                break;
+            case Movement_State.Walk:
+                print("Entered Walk State");
+                break;
+            case Movement_State.Jump:
+                print("Entered Jump State");
+                rb.AddForce(Vector2.up * jump_strength, ForceMode2D.Impulse);
+                break;
+            case Movement_State.Fall:
+                print("Entered Fall State");
+                break;
+            case Movement_State.Crouch:
+                print("Entered Crouch State");
+                playerCollider.size = new Vector2(playerCollider.size.x, crouchHeight);
+                playerCollider.offset = new Vector2(0, -0.5f);
+                break;
         }
+    }
 
-        if(wantsCrouch )
+    void ExitState()
+    {
+        previous_state = current_state;
+        switch (current_state) // This switch statement executes the corresponding lines once exiting a state
         {
-            Crouch();
+            case Movement_State.Idle:
+                print("Exited Idle State");
+                break;
+            case Movement_State.Walk:
+                print("Exited Walk State");
+                break;
+            case Movement_State.Jump:
+                print("Exited Jump State");
+                break;
+            case Movement_State.Fall:
+                print("Exited Fall State");
+                break;
+            case Movement_State.Crouch:
+                print("Exited Crouch State");
+                break;
         }
-        else
+    }
+
+    void UpdateIdleState()
+    {
+        print("Idling");
+
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, 0, caluculate_fricition() * Time.deltaTime);
+
+        if (!isGrounded && rb.linearVelocity.y < 0)
+        {
+            EnterState(Movement_State.Fall);
+        }
+        if (movingInput != Vector2.zero)
+        {
+            EnterState(Movement_State.Walk);
+        }
+        if (jump_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Jump);
+        }
+        if (crouch_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Crouch);
+        }
+    }
+    void UpdateFallState()
+    {
+        print("Falling");
+        if (isGrounded)
+        {
+            EnterState(Movement_State.Idle);
+        }
+    }
+    void UpdateWalkState()
+    {
+        print("Walking");
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, movingInput.x * walk_speed, w_acceleration * Time.deltaTime);
+        if (!isGrounded && rb.linearVelocity.y < 0)
+        {
+            EnterState(Movement_State.Fall);
+        }
+        if (movingInput == Vector2.zero)
+        {
+            EnterState(Movement_State.Idle);
+        }
+        if (jump_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Jump);
+        }
+        if (crouch_action.WasPressedThisFrame())
+        {
+            EnterState(Movement_State.Crouch);
+        }
+    }
+    void UpdateJumpState()
+    {
+        print("Jumping");
+        if (rb.linearVelocity.y < 0)
+        {
+            EnterState(Movement_State.Fall);
+        }
+    }
+    void UpdateCrouchState()
+    {
+        print("Crouching");
+        rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, movingInput.x * crouch_speed, c_acceleration * Time.deltaTime);
+        if (!crouch_action.IsPressed())
         {
             StandAttempt();
         }
-        
-    }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        movingInput = context.ReadValue<Vector2>();
     }
-
-    public void OnCrouch(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            wantsCrouch = true;
-        }
-        else
-        {
-            wantsCrouch = false;
-        }
-    }
-
-    public void Crouch()
-    {
-        playerCollider.size = new Vector2(playerCollider.size.x, crouchHeight);
-        playerCollider.offset = crouchingOffset;
-        speed = 4f;
-    }
-
     public void StandAttempt()
     {
         bool notblocked = !Physics2D.OverlapCircle(headCheck.position, headCheckRadius, groundLayer);
         if (notblocked && !tookDamage)
         {
-            Debug.Log("uncrouched");
             playerCollider.size = new Vector2(playerCollider.size.x, standingHeight);
             playerCollider.offset = standingOffset;
-            speed = 5f;
+            EnterState(Movement_State.Idle);
         }
     }
 
-    public void DamageTaken(Collision2D other) // This is part one of damage recoil where the player gets launched backwards. When there is a ceiling 
+    void DamageTaken(Collision2D other) // This is part one of damage recoil where the player gets launched backwards. I wasn't able to implement it into the player state machine yet.
     {
         Debug.Log("Took Damage");
         tookDamage = true; 
         isGrounded = false;
-        controls.Player.Disable();
+        
+        peeDeeAudio.PlayOneShot(damageTaken);
         playerCollider.size = new Vector2(playerCollider.size.x, crouchHeight); // Did this mainly for when you take damage in a small space.
         playerCollider.offset = crouchingOffset;
         rb.gravityScale = recoilGravity; // Might need adjustment.
         if (headCheck.position.x >= other.transform.position.x) 
         {
             rb.linearVelocity = new Vector2(recoilHorizontalStrength, recoilVerticalStrength);
+            playerTookDamageFromRight = true;
         }
         else
         {
             rb.linearVelocity = new Vector2(recoilHorizontalStrength * -1, recoilVerticalStrength);
+            playerTookDamageFromRight = false;
         }
+        manager.GetComponent<PlayerTreasure>().RemoveAndScatterCoins(playerTookDamageFromRight);
     }
 
-    public void DamageRecovery() // Part 2 of damage recoil, is called when the player hits the ground again.
+    void DamageRecovery() // Part 2 of damage recoil, is called when the player hits the ground again.
     {
         if (tookDamage) 
         {
             tookDamage = false;
-            controls.Player.Enable();
-            rb.gravityScale = standardGravity; 
+            
+            rb.gravityScale = standardGravity;
+            StandAttempt();
+            //manager.GetComponent<PlayerTreasure>().RemoveAndScatterCoins();
         }
     }
-
-    public void OnJump(InputAction.CallbackContext context)
+    float caluculate_fricition()
     {
-        if (context.performed && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            Debug.Log("Jumping");
-        }
+        var normal_force = rb.mass * (Physics2D.gravity.y);
+        var fricition_force = normal_force * friction_coefficient * -1;
+        return fricition_force;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Ground")) 
+        /*if (other.gameObject.CompareTag("Ground")) 
         {
             Debug.Log("Touching ground");
             isGrounded = true;
             blocks++;
+            
             DamageRecovery();
-        }
+        } */
 
         if (other.gameObject.CompareTag("Lose Treasure"))
         {
@@ -165,15 +269,31 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Gain Treasure" && !tookDamage)
+        {
+            TreasureItem treasure = collision.gameObject.GetComponent<TreasureItem>();
+            manager.GetComponent<PlayerTreasure>().AddCoins(treasure.treasureAmount);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        Vector3 normal = other.GetContact(0).normal;
+        print(normal);
+        if (normal.y > 0.8f ) // can't go up angles no more than 36 degrees
+        {
+            isGrounded = true;
+            DamageRecovery();
+        }
+    }
+
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            blocks--;
-            if(blocks == 0)
-            {
-                isGrounded = false;
-            }
+            isGrounded = false;
         }
     }
 }
